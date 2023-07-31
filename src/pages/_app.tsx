54be0ea5853cbbb69718a6d3cb78cd90f7e77db4
stan <winatastanley355@gmail.com>
2023-07-31 08:15:51 +0700

    Some dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import {AppProps} from 'next/app';
+import {useEffect, useState} from 'react';
+import "@/styles/globals.scss";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({Component, pageProps}: AppProps) {
+  const [render, setRender] = useState(false);
+  useEffect(() => setRender(true), [render]);
+  return render ? <Component {...pageProps} /> : null;
 }
+export default App;
