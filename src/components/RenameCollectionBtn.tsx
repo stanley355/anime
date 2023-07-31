ecaed840dc7b39cf2b847ed8a2aecc7f7e53b3f8
stan <winatastanley355@gmail.com>
2023-07-31 19:09:50 +0700

    Rename collection

---
+++ src/components/RenameCollectionBtn.tsx
@@ -0,0 +1,120 @@
+import React, { useState } from 'react';
+import { FaPen } from 'react-icons/fa';
+import { css } from '@emotion/css';
+
+interface IRenameCollectionBtn {
+  collection: any;
+  index: number;
+  onCollectionChange: (col: Array<any>) => void;
+}
+
+const RenameCollectionBtn = (props: IRenameCollectionBtn) => {
+  const { collection, index, onCollectionChange } = props;
+  const [showModal, setShowModal] = useState(false);
+  const [formError, setFormError] = useState("");
+
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setFormError("");
+    const target = e.target as any;
+    const name = target.name.value;
+
+    if (!name) {
+      setFormError("Collection Name can't be empty!");
+      return;
+    }
+
+    const hasSpecialChar = name.match(/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/);
+    if (hasSpecialChar) {
+      setFormError("Collection Name can't include special characters!");
+      return;
+    }
+
+    const collection = {
+      id: name.split(" ").join("-"),
+      name: name,
+      createdAt: new Date(),
+      firstChild: {}
+    };
+
+    const collectionStorage = localStorage.getItem("collections");
+    let oldCollection: Array<any> = JSON.parse(String(collectionStorage));
+    const collectionFilter = oldCollection.filter((col) => col.name === name);
+    if (collectionFilter.length > 0) {
+      setFormError("You already have collection with the same name!");
+      return;
+    }
+
+    oldCollection.splice(index, 1, collection);
+    localStorage.setItem("collections", JSON.stringify(oldCollection));
+    onCollectionChange(oldCollection);
+    setShowModal(false);
+    return;
+  }
+
+
+  return (
+    <div>
+      <button
+        onClick={() => setShowModal(true)}
+        type="button"
+        className={css`display:flex; align-items:center; justify-content:center; gap:4px; width:100%; height:100%; border-radius: 0.25rem; border:none; cursor:pointer;`}
+      >
+        <FaPen />
+        <span>Edit</span>
+      </button>
+      <div className={css`
+        display: ${showModal ? "block" : "none"};
+        background: linear-gradient(rgba(0,0,0, .3), rgba(0,0,0,.3));
+        position:fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+      `}>
+        <div
+          className={css`
+            background:white; 
+            width:95%; 
+            height:fit-content; 
+            margin: 30% auto; 
+            border-radius: 0.25rem; 
+            color: black; 
+            padding: 1rem;
+            @media (min-width: 1024px) { 
+              width: 400px;
+              margin: 15% auto;
+            }
+          `}>
+          <div className={css`text-align:center; font-size: 28px; font-weight: bold; margin-bottom: 2rem;`}>
+            Rename
+            <span className={css`text-decoration: underline; margin: 0 4px;`}>
+              {collection.name}
+            </span>
+            Collection
+          </div>
+          <div className={css`text-align:center;`}>What's the new Collection Name</div>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="name">
+              <input type="text" id='name' placeholder='Glorious Purpose' className={css`width: 100%; padding:0.5rem; font-size: 18px; border-radius: 0.25rem;`} />
+            </label>
+            <div className={css`color:red;`}>{formError}</div>
+            <button type="submit" className={css`width:100%; cursor: pointer; padding: 0.5rem; margin-top: 1rem; background: linear-gradient(to bottom, #90dffe 0%,#38a3d1 100%); border-radius: 0.25rem; font-size: 18px; font-weight: bold;`}>
+              Submit
+            </button>
+            <button
+              type="button"
+              onClick={() => setShowModal(false)}
+              className={css`text-align: center; cursor:pointer; padding: 0.5rem; margin-top: 1rem; background: none; width: 100%; font-size:16px; border:none;`}>
+              I'll change later
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default RenameCollectionBtn;
--- src/pages/collection/index.tsx
+++ src/pages/collection/index.tsx
@@ -5,6 +5,7 @@ import AddNewCollectionBtn from '@/components/AddNewCollectionBtn';
 import PikaIsSad from '@/components/PikaIsSad';
 import { Link } from 'react-router-dom';
 import { FaEye, FaPen, FaTrash } from 'react-icons/fa';
+import RenameCollectionBtn from '@/components/RenameCollectionBtn';
 
 const MyCollection = () => {
   const collectionStorage = localStorage.getItem("collections");
@@ -15,7 +16,9 @@ const MyCollection = () => {
     <div className={css`
       min-height:100vh;
       padding: 0.5rem;
-      background: linear-gradient(to bottom, #82addb 0%,#ebb2b1 100%);
+      background: url("collection_bg.webp");
+      background-repeat: no-repeat;
+      background-size: cover;
       @media (min-width: 1024px) { 
         max-width: 1024px;
         margin: auto
@@ -34,8 +37,9 @@ const MyCollection = () => {
               gap:0.5rem;
             }
       `}>
-        {collections.length > 0 && collections.map((col: any) => <div key={col.id}
-          className={css`
+        {collections.length > 0 &&
+          collections.map((col: any, index: number) => <div key={col.id}
+            className={css`
           margin-bottom: 1rem;
           border-radius: 0.25rem; 
           padding: 0.5rem;
@@ -44,15 +48,15 @@ const MyCollection = () => {
           background-size: cover;
           background-position: fixed;
         `}>
-          <img src="placeholder.webp" alt={col.name} width={300} height={300} className={css`width:100%; height: 150px; border-radius: 0.25rem;`} />
-          <div className={css`display:flex; align-items:center; justify-content: space-between;`}>
-            <Link to={`/collections/${col.id}`} className={css`font-size:20px; font-weight:bold;&:hover{text-decoration: underline;}`}>{col.name}</Link>
-            <div>Created At: {new Date(col.createdAt).toLocaleDateString()}</div>
-          </div>
-          <div className={css`display:grid; grid-template-columns: 33% 33% 33%; gap: 4px;margin-top:1rem;`}>
-            <Link
-              to={`/collections/${col.id}`}
-              className={css`
+            <img src="placeholder.webp" alt={col.name} width={300} height={300} className={css`width:100%; height: 150px; border-radius: 0.25rem;`} />
+            <div className={css`display:flex; align-items:center; justify-content: space-between;`}>
+              <Link to={`/collections/${col.id}`} className={css`font-size:20px; font-weight:bold;&:hover{text-decoration: underline;}`}>{col.name}</Link>
+              <div>{new Date(col.createdAt).toLocaleDateString()}</div>
+            </div>
+            <div className={css`display:grid; grid-template-columns: 33% 33% 33%; gap: 4px;margin-top:1rem;`}>
+              <Link
+                to={`/collections/${col.id}`}
+                className={css`
                 display:flex; 
                 align-items:center; 
                 justify-content:center; 
@@ -62,20 +66,17 @@ const MyCollection = () => {
                 color:black; 
                 border-radius:0.25rem;
               `}>
-              <FaEye />
-              <span>View</span>
-            </Link>
-            <button type="button">
-              <FaPen />
-              <span>Rename</span>
-            </button>
-            <button type="button">
-              <FaTrash />
-              <span>Dump</span>
-            </button>
+                <FaEye />
+                <span>View</span>
+              </Link>
+              <RenameCollectionBtn collection={col}  index={index} onCollectionChange={setCollections} />
+              <button type="button">
+                <FaTrash />
+                <span>Dump</span>
+              </button>
+            </div>
           </div>
-        </div>
-        )}
+          )}
       </div>
     </div>
   )
