86305ff1b228a0ca43d0d230a108655411240001
stan <winatastanley355@gmail.com>
2023-08-01 14:39:07 +0700

    Collection modal done

--- src/components/CollectionList.tsx
+++ src/components/CollectionList.tsx
@@ -22,31 +22,37 @@ const CollectionList = (props: ICollectionList) => {
         gap: 1rem;
       }`
     }>
-      {collections.map((col: any, index: number) => <div key={col.id}
-        className={css`
+      {collections.map((col: any, index: number) => {
+        const colStorage = localStorage.getItem(col.id);
+        const parsedCol = colStorage ? JSON.parse(String(colStorage)) : [];
+        const avatar = parsedCol.length > 0 ? parsedCol[0].coverImage.large : "placeholder.webp";
+
+        return (
+          <div key={col.id}
+            className={css`
             display:flex;
             gap: 0.5rem;
             margin-bottom: 1rem;
             border: 1px solid white;
             border-radius: 0.25rem; 
             padding: 0.5rem;`
-        }>
-        <Link to={`/collections/${col.id}`} className={css`width:40%; height:150px;`}>
-          <img src="placeholder.webp" alt={col.name} width={300} height={300} className={css`width:100%; height: 100%; border-radius: 0.25rem;`} />
-        </Link>
-        <div className={css`width: 100%;`}>
-          <div className={css`color: black;`}>
-            <Link to={`/collections/${col.id}`} className={css`font-size:20px; font-weight:bold; background: white; padding: 0.25rem; border-radius: 0.25rem; &:hover {text-decoration: underline;}`}>
-              {col.name}
+            }>
+            <Link to={`/collections/${col.id}`} className={css`width:40%; height:150px;`}>
+              <img src={avatar} alt={col.name} width={300} height={300} className={css`width:100%; height: 100%; border-radius: 0.25rem;`} />
             </Link>
-            <div className={css`font-size:16px; background: white; padding: 0.25rem; border-radius: 0.25rem; margin-top: 0.5rem; width: fit-content;`}>
-              {new Date(col.createdAt).toLocaleDateString()}
-            </div>
-          </div>
-          <div className={css`display:grid; grid-template-columns: 33% 33% 33%; gap: 4px;margin-top: 3.25rem;`}>
-            <Link
-              to={`/collections/${col.id}`}
-              className={css`
+            <div className={css`width: 100%;`}>
+              <div className={css`color: black;`}>
+                <Link to={`/collections/${col.id}`} className={css`font-size:20px; font-weight:bold; background: white; padding: 0.25rem; border-radius: 0.25rem; &:hover {text-decoration: underline;}`}>
+                  {col.name}
+                </Link>
+                <div className={css`font-size:16px; background: white; padding: 0.25rem; border-radius: 0.25rem; margin-top: 0.5rem; width: fit-content;`}>
+                  {new Date(col.createdAt).toLocaleDateString()}
+                </div>
+              </div>
+              <div className={css`display:grid; grid-template-columns: 33% 33% 33%; gap: 4px;margin-top: 3.25rem;`}>
+                <Link
+                  to={`/collections/${col.id}`}
+                  className={css`
                 display:flex; 
                 align-items:center; 
                 justify-content:center; 
@@ -55,15 +61,17 @@ const CollectionList = (props: ICollectionList) => {
                 padding: 0.5rem; 
                 color:black; 
                 border-radius:0.25rem;`
-              }>
-              <FaEye />
-              <span>View</span>
-            </Link>
-            <RenameCollectionBtn collection={col} index={index} onCollectionChange={onCollectionChange} />
-            <RemoveCollectionBtn collection={col} index={index} onCollectionChange={onCollectionChange} />
+                  }>
+                  <FaEye />
+                  <span>View</span>
+                </Link>
+                <RenameCollectionBtn collection={col} index={index} onCollectionChange={onCollectionChange} />
+                <RemoveCollectionBtn collection={col} index={index} onCollectionChange={onCollectionChange} />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        )
+      }
       )}
     </div>
   )
