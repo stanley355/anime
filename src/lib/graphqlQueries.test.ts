926e0770ad88c26d32cad106922730ba5a6d8ac5
stan <winatastanley355@gmail.com>
2023-08-02 10:29:17 +0700

    Fix test not runnign

--- src/lib/graphqlQueries.test.ts
+++ src/lib/graphqlQueries.test.ts
@@ -1,7 +1,12 @@
 import { describe, expect, test } from "@jest/globals";
+import { fetchAniList } from "./fetchAniList";
+import { HOME_PAGE_QUERIES } from "./graphqlQueries";
 
-describe("sum module", () => {
-  test("adds 1 + 2 to equal 3", () => {
-    expect(1 + 1).toBe(3);
+describe("Test HomePageQueries", () => {
+  test("adds 1 + 2 to equal 3", async () => {
+    const homePageAnimes = await fetchAniList(HOME_PAGE_QUERIES, {
+      variables: { page: 1 },
+    });
+    expect(homePageAnimes).toBe(3);
   });
 });
